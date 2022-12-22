3edcbd2dcf32810b29840c8e6df2da45102837e5
Fabio Caprara <fats140701@gmail.com>
2022-12-22 19:28:27 -0400

    feat: add cors in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
+
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 const port = 4000 || process.env.PORT;
 app.listen(port, () => {
